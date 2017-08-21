b62ac0629f91c29adc992abbf4777cf5bfc427a5
AddardaEtafira <etafiraa@outlook.fr>
2017-08-21 12:06:29 +0200

    exo-7 btn cree btn

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -93,8 +93,28 @@ var btn = section7.querySelectorAll('button');
 
 //supprimer un enfant
 var supprimerElement = function supprimerElement() {
-    var parent = this.parentElement;
-    parent.removeChild(this);
+    // //recup parent
+    // let parent = this.parentElement;
+    // //le parent supprime l'enfant
+    // parent.removeChild(this);
+    //creation d'un span
+    var createSpan = document.createElement("span");
+    createSpan.style.width = "95px";
+    createSpan.style.display = "inline-block";
+    //echanger la place entre le span et le button
+    this.parentNode.replaceChild(createSpan, this);
+
+    //creation d'un button
+    var createBtn = document.createElement("button");
+    // Mettre du texte dans l'élément li
+    var contenu = document.createTextNode("delete me");
+    createBtn.appendChild(contenu);
+    //ajouter une classe
+    createBtn.className = "btn btn-success";
+    //ajouter dans le parent
+    section7.appendChild(createBtn);
+    //ajouter un ecouteur d'evenement
+    createBtn.addEventListener('click', supprimerElement);
 };
 
 //ecouteur d'evement
--- src/js/exo/exo-7.js
+++ src/js/exo/exo-7.js
@@ -4,8 +4,29 @@ let btn = section7.querySelectorAll('button');
 
 //supprimer un enfant
 let supprimerElement = function () {
-    let parent = this.parentElement;
-    parent.removeChild(this);
+    // //recup parent
+    // let parent = this.parentElement;
+    // //le parent supprime l'enfant
+    // parent.removeChild(this);
+    //creation d'un span
+    let createSpan = document.createElement("span");
+    createSpan.style.width = "95px";
+    createSpan.style.display = "inline-block";
+    //echanger la place entre le span et le button
+    this.parentNode.replaceChild(createSpan, this);
+
+    //creation d'un button
+    let createBtn = document.createElement("button");
+    // Mettre du texte dans l'élément li
+    let contenu = document.createTextNode("delete me");
+    createBtn.appendChild(contenu);
+    //ajouter une classe
+    createBtn.className = "btn btn-success";
+    //ajouter dans le parent
+    section7.appendChild(createBtn);
+    //ajouter un ecouteur d'evenement
+    createBtn.addEventListener('click', supprimerElement)
+
 }
 
 //ecouteur d'evement
